737ab99ff12dd92414065e241865e8689d299b6f
Valentyna Svistunova <svistunova.valentyna@gmail.com>
2026-02-18 16:48:43 +0300

    Updated unit tests

--- src/test/java/filerenamer/TemplateNamingStrategyTest.java
+++ src/test/java/filerenamer/TemplateNamingStrategyTest.java
@@ -3,6 +3,8 @@ package filerenamer;
 import fileprocessor.FileMetadata;
 import junit.framework.TestCase;
 
+import java.time.LocalDateTime;
+
 public class TemplateNamingStrategyTest extends TestCase {
 
     public void testProcessOriginalName() {
@@ -16,9 +18,11 @@ public class TemplateNamingStrategyTest extends TestCase {
         // multiple matches
         assertEquals("testimage-001_TestI", TemplateNamingStrategy.processOriginalName("{original:lower}_{original:5}", metadata));
 
-        // invalid formats
+        // invalid patterns
         assertEquals("TestImage-001", TemplateNamingStrategy.processOriginalName("{original:lowe10}", metadata));
         assertEquals("TestImage-001", TemplateNamingStrategy.processOriginalName("{original::}", metadata));
+
+        assertEquals("{original:lower|default}", TemplateNamingStrategy.processOriginalName("{original:lower|default}", metadata));
 
         assertEquals("TestImage-001", TemplateNamingStrategy.processOriginalName("{original}", metadata));
         assertEquals("TESTIMAGE-001", TemplateNamingStrategy.processOriginalName("{original:upper}", metadata));
@@ -31,5 +35,46 @@ public class TemplateNamingStrategyTest extends TestCase {
         assertEquals("testi", TemplateNamingStrategy.processOriginalName("{original:5:lower}", metadata));
         assertEquals("TestI", TemplateNamingStrategy.processOriginalName("{original:5:10}", metadata));
         assertEquals("testimage-", TemplateNamingStrategy.processOriginalName("{original:lower:10}", metadata));
+    }
+
+    public void testProcessDateTime() {
+        FileMetadata metadata = new FileMetadata();
+        metadata.setOriginalName("TestImage-001");
+        metadata.setExtension("jpg");
+        metadata.setDateTime(LocalDateTime.of(2025, 1, 20, 14,45, 0));
+
+        assertEquals("2025.01.20_14-45", TemplateNamingStrategy.processDateTime("{date}", metadata));
+        assertEquals("2025", TemplateNamingStrategy.processDateTime("{date:yyyy}", metadata));
+        assertEquals("25", TemplateNamingStrategy.processDateTime("{date:yy}", metadata));
+        assertEquals("2025-01-20", TemplateNamingStrategy.processDateTime("{date:yyyy-MM-DD}", metadata));
+        assertEquals("2025-01-20", TemplateNamingStrategy.processDateTime("{date:yyyy-MM-dd}", metadata));
+        assertEquals("20250120-024500", TemplateNamingStrategy.processDateTime("{date:yyyyMMdd-hhmmss}", metadata));
+        assertEquals("20250120-144500", TemplateNamingStrategy.processDateTime("{date:yyyyMMdd-HHmmss}", metadata));
+        assertEquals("date is 01/20/2025 and time is 14-45", TemplateNamingStrategy.processDateTime("date is {date:MM/dd/yyyy} and time is {date:HH-mm}", metadata));
+    }
+
+    public void testProcessDateTimeWhenNoExif() {
+        FileMetadata metadata = new FileMetadata();
+        metadata.setOriginalName("TestImage-001");
+        metadata.setExtension("jpg");
+        metadata.setDateTime(null);
+
+        assertEquals("nodate", TemplateNamingStrategy.processDateTime("{date}", metadata));
+        assertEquals("nodate", TemplateNamingStrategy.processDateTime("{date:yyyy}", metadata));
+        assertEquals("nodate", TemplateNamingStrategy.processDateTime("{date:yyyy:lower}", metadata));
+
+        assertEquals("noexif", TemplateNamingStrategy.processDateTime("{date|noexif}", metadata));
+        assertEquals("no exif", TemplateNamingStrategy.processDateTime("{date|no exif}", metadata));
+
+        // empty default value
+        assertEquals("", TemplateNamingStrategy.processDateTime("{date|}", metadata));
+
+        // invalid patterns - empty format
+        assertEquals("{date:}", TemplateNamingStrategy.processDateTime("{date:}", metadata));
+
+        // invalid patterns - multiple default values
+        assertEquals("{date:yyyyy|noexif1|noexif2}", TemplateNamingStrategy.processDateTime("{date:yyyyy|noexif1|noexif2}", metadata));
+        assertEquals("{date|noexif1|noexif2}", TemplateNamingStrategy.processDateTime("{date|noexif1|noexif2}", metadata));
+
     }
 }
